cb0908630fe4c3a7b3ef38573e10776cf58c74f9
thartbm <thartbm@gmail.com>
2022-02-14 19:47:00 -0500

    app fits, plots and displays table locally

---
+++ PReMo/R/PReMo.R
@@ -0,0 +1,185 @@
+
+PReMo_model <- function(parfree, parset, schedule) {
+  
+  # we want all the parameters in 1 vector:
+  p <- c(parfree, parset)
+  
+  # so that we can read them out as variables here:
+  var_u     <- p['var_u']
+  var_p     <- p['var_p']
+  var_v     <- p['var_v']
+  eta_p     <- p['eta_p']
+  eta_v     <- p['eta_v']
+  beta_psat <- p['beta_psat']
+  beta_vsat <- p['beta_vsat']
+  K         <- p['K']
+  
+  # initialize the state:
+  x_pt    <- 0
+  x_vt    <- 0
+  beta_pt <- 0
+  beta_vt <- 0
+  Gt      <- 0 # this will be kept 0, could increase for explicit re-aiming
+  
+  # empty vectors:
+  reachdev  <- c()
+  proprecal <- c()
+
+  for (trial in c(1:dim(schedule)[1])) {
+    
+    # Marius' added line:
+    x_vt <- switch(schedule$trialtype[trial],
+                   x_pt + schedule$rotation[trial],
+                   schedule$rotation[trial])
+    
+    # Eq. 1
+    # integrated proprioceptive position:
+    xI_pt <- ( (var_u/(var_u+var_p)) * x_pt ) + ( (var_p/(var_u+var_p)) * Gt )
+    
+    # Eq. 2
+    # integrated visual position:
+    xI_vt <-  ( (var_u/(var_u+var_v)) * x_vt ) + ( (var_p/(var_u+var_v)) * Gt )
+    
+    # Eq. 3
+    # perceived proprioceptive position:
+    xper_pt <- xI_pt + beta_pt
+    
+    # Eq. 4
+    # perceived visual position:
+    xper_vt <- xI_vt + beta_vt
+    
+    # Eq. 5
+    # reported proprioceptive shift:
+    beta_pt <- min(eta_p*(xI_vt - xI_pt), beta_psat)
+    
+    # Eq. 6
+    # reported visual shift:
+    beta_vt <- min(eta_p*(xI_vt - xI_pt), beta_vsat)
+    
+    
+    # store stuff in vectors:
+    reachdev  <- c(reachdev,  x_pt)
+    proprecal <- c(proprecal, beta_pt)
+    
+    # Eq. 8 (and 7)
+    # reach direction on next trial:
+    x_pt <- x_pt + (K*(Gt - xper_pt))
+    
+  }
+  
+  return(data.frame(reachdev, proprecal))
+  
+}
+
+
+
+PReMo_errors <- function(parfree, parset, schedule, dataset) {
+  
+  # this will compare model output to reaches, or proprioception, or both
+  # but not neither
+  
+  if ( !any(c('reachdev', 'proprecal') %in% names(dataset)) ) {
+    cat('can not evaluate model errors if there is no data\n')
+  }
+  # can do more checks, such as that if these variables are not null they are: 
+  # - some kind of numeric vector
+  # - with length equal to schedule
+  
+  # run model with current parameters:
+  prediction <- PReMo_model(parfree, parset, schedule)
+  
+  # collect errors here:
+  errors <- c()
+  
+  for (varname in c('reachdev', 'proprecal')) {
+    if (varname %in% names(dataset)) {
+      errors <- c(errors, prediction[,varname] - dataset[,varname])
+    }
+  }
+  
+  # remove NAs
+  errors <- errors[!is.na(errors)]
+  
+  return(sum(errors^2))
+  
+}
+
+
+library(optimx)
+
+PReMo_fit <- function(dataschedule, 
+                      parset=c(),
+                      npoints=6,
+                      maxgridsize=250,
+                      gridtop=5,
+                      parlimits = NULL) {
+  
+  # split dataschedule:
+  dataset  <- dataschedule[,which(names(dataschedule) %in% c('reachdev','proprecal'))]
+  schedule <- dataschedule[,which(names(dataschedule) %in% c('rotation','trialtype'))]
+  
+  
+  # start search grid for free parameters:
+  searchseqs <- list()
+  lower <- c()
+  upper <- c()
+  
+  # if not set, these are the default limits:
+  if (is.null(parlimits)) {
+    parlimits <- data.frame('parameter'=c('K',  'eta_p', 'eta_v', 'var_u', 'var_p', 'var_v', 'beta_vsat', 'beta_psat'),
+                            'lo'       =c(0.001, 0.001,  0.001,    0.001,    0.001,   0.001,   0.001,       0.001),
+                            'hi'       =c(0.999, 0.999,  0.999,   20,       20,       5,      10,          15))
+  }
+  
+  for (rown in c(1:dim(parlimits)[1])) {
+    if (parlimits$parameter[rown] %in% names(parset)) { next() }
+    lo <- parlimits[rown,'lo']
+    hi <- parlimits[rown,'hi']
+    if (lo == hi) {
+      thisparval <- c(lo)
+      names(thisparval) <- c(parlimits$parameter[rown])
+      parset <- c(parset, thisparval)
+    } else {
+      step <- diff(c(lo,hi))/npoints
+      searchseqs[[parlimits$parameter[rown]]] <- seq(lo+(step/2),hi-(step/2),step)
+      lower <- c(lower, lo)
+      upper <- c(upper, hi)
+    }
+  }
+  
+  print(parset)
+  
+  searchgrid <- expand.grid(searchseqs)
+  
+  if (dim(searchgrid)[1] > maxgridsize) {
+    searchgrid <- searchgrid[sample(c(1:dim(searchgrid)[1]),size=maxgridsize),]
+  }
+  
+  MSEs <- apply(searchgrid,FUN=PReMo_errors,MARGIN=c(1),parset=parset,schedule=schedule,dataset=dataset)
+  
+  topgrid <- searchgrid[order(MSEs)[c(1:gridtop)],]
+  
+  # do fitting on the best results from the search grid:
+  topFits <- do.call("rbind",
+                     apply( topgrid,
+                            MARGIN=c(1),
+                            FUN=optimx::optimx,
+                            fn=PReMo_errors,
+                            method='L-BFGS-B',
+                            lower=lower,
+                            upper=upper,
+                            schedule=schedule,
+                            parset=parset,
+                            dataset=dataset) )
+  
+  winFit <- topFits[order(topFits$value)[1],]
+  
+  idx <- which(names(winFit) == 'value') - 1
+  
+  winpar <- as.numeric(winFit[c(1:(idx))])
+  names(winpar) <- names(winFit[c(1:(idx))])
+  
+  return(c(winpar,parset))
+  
+}
+
--- PReMo/app.R
+++ PReMo/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(optimx)
+library(graphics)
 
 # Define UI: data/schedule and model parameters
 ui <- fluidPage(
@@ -16,6 +17,7 @@ ui <- fluidPage(
     
     tags$a(href="https://doi.org/10.1101/2021.12.21.473747", 
            "Implementation of the model in Tsay et al. (2021; bioRxiv)."),
+    p("Currently only implements fully implicit adaptation."),
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -31,15 +33,15 @@ ui <- fluidPage(
             sliderInput("beta_vast_slider", "beta_vsat", min = 0.100, max = 10.00, step=0.100, value = c(0.100, 10.00)),
             sliderInput("beta_psat_slider", "beta_psat", min = 0.100, max = 15.00, step=0.100, value = c(0.100, 15.00)),
             
-            actionButton("fit", "Fit model to data", class = "btn-lg btn-success"),
+            actionButton("fit", "Fit model to data", class = "btn-lg btn-success", icon=icon("gears"))
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
-            p("Data input needs to be a .csv or .tsv file, where rows are trials, and with 2 columns defining the experiment: `rotation` (with degrees rotation) and `trialtype` either 1: normal cursor, or 2: error-clamped feedback. It also need to have at least one of two columns: `reachdev` with reach deviations (in degrees) or `proprecal` with proprioceptive estimates. All measures relative to the target."),
-            p("Parameters can be set by making the minimum and maximum the same. If not set, they will be fit using the minimum and maximum as lower and upper limits.")
-            # plotOutput("distPlot")
-            # tableOutput("fittedParameters")
+            p("File (.csv or .tsv), where rows=trials, 2 columns define the experiment: `rotation` and `trialtype` (1: normal, 2: error-clamped). Also one of two columns: `reachdev` (reach deviations) or `proprecal` (proprioceptive estimates). Measures relative to target in the same unit."),
+            p("Parameters: free (fit in the range), or fixed (when minimum == maximum)."),
+            plotOutput("fittedDataPlot"),
+            tableOutput("fittedParameters")
         )
     )
 )
@@ -47,26 +49,132 @@ ui <- fluidPage(
 # Define server logic 
 server <- function(input, output) {
     
-    dataschedule <- reactive({
-        req(input$upload)
-        
+    source("R/PReMo.R")
+    
+    # uploaded data: schedule + actual data
+    # dataframe of parameters
+    
+    parlimits <- data.frame('parameter'=c('K',  'eta_p', 'eta_v', 'var_u', 'var_p', 'var_v', 'beta_vsat', 'beta_psat'),
+                            'lo'       =c(0.001, 0.001,  0.001,    0.001,    0.001,   0.001,   0.001,       0.001),
+                            'hi'       =c(0.999, 0.999,  0.999,   20,       20,       5,      10,          15))
+    
+    model <- reactiveValues(dataschedule=NULL, parlimits=parlimits, parfitted=NULL)
+    
+    observeEvent(input$upload, {
         ext <- tools::file_ext(input$upload$name)
-        switch(ext,
-               csv = read.csv(input$upload$datapath, sep = ","),
-               tsv = read.csv(input$upload$datapath, sep = "\t"),
-               validate("Invalid file; Please upload a .csv or .tsv file")
+        model$dataschedule <- switch(ext,
+                                     csv = read.csv(input$upload$datapath, sep = ","),
+                                     tsv = read.csv(input$upload$datapath, sep = "\t"),
+                                     validate("Invalid file; Please upload a .csv or .tsv file")
         )
     })
     
     
+    observeEvent(input$fit, {
+        validate(
+            need(!is.null(model$dataschedule), "Please load some data to select.")
+        )
+        
+        # this takes some time:
+        fit <- PReMo_fit(dataschedule = model$dataschedule,
+                         parlimits = model$parlimits)
+        model$parfitted <- data.frame('parameter' = names(fit),
+                                      'value' = as.numeric(fit))
+        
+        
+        print(model$parfitted)
+    })
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    observeEvent(input$K_slider, {
+        idx <- which(model$parlimits$parameter == 'K')
+        model$parlimits$lo[idx] <- input$K_slider[1]
+        model$parlimits$hi[idx] <- input$K_slider[2]
+    })
+    
+    observeEvent(input$eta_p_slider, {
+        idx <- which(model$parlimits$parameter == 'eta_p')
+        model$parlimits$lo[idx] <- input$eta_p_slider[1]
+        model$parlimits$hi[idx] <- input$eta_p_slider[2]
+    })
+    
+    observeEvent(input$eta_v_slider, {
+        idx <- which(model$parlimits$parameter == 'eta_v')
+        model$parlimits$lo[idx] <- input$eta_v_slider[1]
+        model$parlimits$hi[idx] <- input$eta_v_slider[2]
+    })
+    
+    observeEvent(input$var_u_slider, {
+        idx <- which(model$parlimits$parameter == 'var_u')
+        model$parlimits$lo[idx] <- input$var_u_slider[1]
+        model$parlimits$hi[idx] <- input$var_u_slider[2]
+    })
+    
+    observeEvent(input$var_p_slider, {
+        idx <- which(model$parlimits$parameter == 'var_p')
+        model$parlimits$lo[idx] <- input$var_p_slider[1]
+        model$parlimits$hi[idx] <- input$var_p_slider[2]
+    })
+    
+    observeEvent(input$var_v_slider, {
+        idx <- which(model$parlimits$parameter == 'var_v')
+        model$parlimits$lo[idx] <- input$var_v_slider[1]
+        model$parlimits$hi[idx] <- input$var_v_slider[2]
+    })
+    
+    observeEvent(input$beta_vsat_slider, {
+        idx <- which(model$parlimits$parameter == 'beta_vsat')
+        model$parlimits$lo[idx] <- input$beta_vsat_slider[1]
+        model$parlimits$hi[idx] <- input$beta_vsat_slider[2]
+    })
+    
+    observeEvent(input$beta_psat_slider, {
+        idx <- which(model$parlimits$parameter == 'beta_psat')
+        model$parlimits$lo[idx] <- input$beta_psat_slider[1]
+        model$parlimits$hi[idx] <- input$beta_psat_slider[2]
+    })
+    
+    
+    output$fittedDataPlot <- renderPlot({
+        
+        req(model$parfitted)
+        
+        # convert fitted par data frame to vector:
+        parfitted <- model$parfitted$value
+        names(parfitted) <- model$parfitted$parameter
+        
+        # run model on schedule & fitted parameters
+        modelfit <- PReMo_model(parfree=parfitted,
+                                parset=c(),
+                                schedule=model$dataschedule)
+        
+        # determine range of Y values in graph:
+        rangevalues <- c(range(dataschedule$rotation),
+                         range(modelfit$reachdev),
+                         range(modelfit$proprecal))
+        if ('reachdev' %in% names(model$dataschedule))  { rangevalues <- c(rangevalues, range(model$dataschedule$reachdev))}
+        if ('proprecal' %in% names(model$dataschedule)) { rangevalues <- c(rangevalues, range(model$dataschedule$proprecal))}
+        
+        # make plot:
+        plot(-1000,-1000,
+             main='',xlab='trial',ylab='deviation',
+             xlim=c(0,(dim(dataschedule)[1]+1)),ylim=range(rangevalues),
+             bty='n'
+             )
+        # add schedule:
+        lines(dataschedule$rotation)
+        
+        # add data:
+        if ('reachdev' %in% names(model$dataschedule))  { lines(model$dataschedule$reachdev,  col='#0fd2e2ff') }
+        if ('proprecal' %in% names(model$dataschedule)) { lines(model$dataschedule$proprecal, col='#ff8200ff') }
+        
+        # add model processes:
+        lines(modelfit$reachdev,  col='#005de4ff')
+        lines(modelfit$proprecal, col='#e51636ff')
+        
+    })
+    
+    output$fittedParameters <- renderTable({
+        model$parfitted
     })
     
 }
